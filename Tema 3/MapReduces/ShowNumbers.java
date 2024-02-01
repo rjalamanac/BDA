70664a66b09e38a436bf7601b593bdec64a2f17a
ProfVespertino <ProfVespertino@PROFESOR14.informatica.comercio>
2024-02-01 16:15:33 +0100

    changes dockerfile + conf

--- Tema 3/MapReduces/ShowNumbers.java
+++ Tema 3/MapReduces/ShowNumbers.java
@@ -8,17 +8,15 @@ import org.apache.hadoop.io.*;
 import org.apache.hadoop.util.*;
 import org.apache.hadoop.fs.*;
 
-
 public class ShowNumbers {
 
+	public static boolean isNumeric(String str) {
+		return str.matches("-?\\d+(\\.\\d+)?");
+	}
 
-public static boolean isNumeric(String str) {
-  return str.matches("-?\\d+(\\.\\d+)?");  
-}
-
-
-public static class Map extends Mapper<LongWritable, Text, Text, Text> {
+	public static class Map extends Mapper<LongWritable, Text, Text, Text> {
 		private Text word = new Text();
+
 		@Override
 		public void map(LongWritable key, Text value, Context context)
 				throws IOException, InterruptedException {
@@ -26,36 +24,36 @@ public static class Map extends Mapper<LongWritable, Text, Text, Text> {
 			StringTokenizer tokenizer = new StringTokenizer(line);
 			while (tokenizer.hasMoreTokens()) {
 				word.set(tokenizer.nextToken());
-        if (isNumeric(word.toString()))
-        {
-				  context.write(word, value);
-        }
+				if (isNumeric(word.toString())) {
+					context.write(word, value);
+				}
 			}
 		}
-    }
+	}
 
-    public static class Reduce extends Reducer<Text, Text, Text, Text> {
-    
-    private Text result = new Text();
+	public static class Reduce extends Reducer<Text, Text, Text, Text> {
+
+		private Text result = new Text();
 
 		@Override
 		public void reduce(Text key, Iterable<Text> values, Context context)
 				throws IOException, InterruptedException {
-      String values_ = "";
-			for (Text val: values) {
-				values_ +=  val.toString();
+			String values_ = "";
+			for (Text val : values) {
+				values_ += val.toString();
 			}
 			result.set(values_);
-      context.write(key, result);
+			context.write(key, result);
 		}
 	}
-    public static void main(String[] args) throws Exception {
 
-        if (args.length < 2) {
-            System.out.println("ShowNumbers <nameFile> <outDir>");
-            ToolRunner.printGenericCommandUsage(System.out);
-            return;
-        }
+	public static void main(String[] args) throws Exception {
+
+		if (args.length < 2) {
+			System.out.println("ShowNumbers <nameFile> <outDir>");
+			ToolRunner.printGenericCommandUsage(System.out);
+			return;
+		}
 		Job job = Job.getInstance();
 		job.setJarByClass(ShowNumbers.class);
 		job.setJobName("ShowNumbers");
@@ -73,6 +71,6 @@ public static class Map extends Mapper<LongWritable, Text, Text, Text> {
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-		System.exit(job.waitForCompletion(true)? 0 : 1);
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
