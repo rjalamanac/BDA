6aed2145ebeea573444614d9643900f9085dd604
rjalamanac <rjalamanac@iescomercio.com>
2025-03-20 16:11:08 +0100

    Spark cluster update

--- Tema 4/Spark/scripts/start-spark.sh
+++ Tema 4/Spark/scripts/start-spark.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
+. "$SPARK_HOME/bin/load-spark-env.sh"
 
-. "/opt/spark/bin/load-spark-env.sh"
-
-if [ "$SPARK_WORKLOAD" == "master" ];
-then
-
-export SPARK_MASTER_HOST=`hostname`
-
-cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG
-
-elif [ "$SPARK_WORKLOAD" == "worker" ];
-then
-
-cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG
-
-elif [ "$SPARK_WORKLOAD" == "submit" ];
-then
-    echo "SPARK SUBMIT"
-else
-    echo "Undefined Workload Type $SPARK_WORKLOAD, must specify: master, worker, submit"
-fi
+case "$SPARK_WORKLOAD" in
+  master)
+    echo "Starting Spark Master..."
+    export SPARK_MASTER_HOST=$(hostname)
+    cd "$SPARK_HOME/bin" && exec ./spark-class org.apache.spark.deploy.master.Master \
+      --ip "$SPARK_MASTER_HOST" --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG
+    ;;
+  worker)
+    echo "Starting Spark Worker..."
+    cd "$SPARK_HOME/bin" && exec ./spark-class org.apache.spark.deploy.worker.Worker \
+      --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG
+    ;;
+  history-server)
+    echo "Starting Spark History Server..."
+    cd "$SPARK_HOME/sbin" && ./start-history-server.sh
+    ;;
+  *)
+    echo "Invalid workload type: $SPARK_WORKLOAD"
+    echo "Valid options: master, worker, submit, history-server"
+    exit 1
+    ;;
+esac
